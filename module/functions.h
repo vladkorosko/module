3c757e18b3fa90bc20d598ea2b43a07323e4db14
vladkorosko <54815768+vladkorosko@users.noreply.github.com>
2019-09-12 19:38:22 +0300

    Create functions.h

---
+++ module/functions.h
@@ -0,0 +1,33 @@
+#pragma once
+int integer10(int a);
+int integer20();
+int integer29_1(int a, int b, int c);
+int integer29_2(int a, int b, int c, int n);
+int integer30();
+
+void integercout();
+void rosdilcout();
+void begincout();
+void booleancout();
+void knowcout();
+void ifcout();
+
+void begin10();
+int begin20();
+double begin30();
+double begin40_1(double a1, double b1, double c1, double a2, double b2, double c2);
+double begin40_2(double a1, double b1, double c1, double a2, double b2, double c2);
+
+void boolean10();
+void boolean20();
+void boolean39();
+void boolean40();
+
+void solution1();
+void solution2();
+void solution3();
+void solution4();
+
+void if10();
+void if20();
+void if30();
