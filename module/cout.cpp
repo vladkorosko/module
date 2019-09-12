85d19a611ddc10cc3494c525814c6ef0c65dc3bd
vladkorosko <54815768+vladkorosko@users.noreply.github.com>
2019-09-12 19:37:54 +0300

    Create cout.cpp

---
+++ module/cout.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "Functions.h"
+
+using namespace std;
+
+void integercout()
+{
+	cout << "What task you want to solve?" << endl;
+	cout << "If you want solve task 'Integer 10', enter 10 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Integer 20', enter 20 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Integer 29', enter 29 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Integer 30', enter 30 and then 'Enter'." << endl;
+}
+
+void rosdilcout()
+{
+	cout << "What topic do you want to work with" << endl;
+	cout << "If you want to work with Begin, enter 1" << endl;
+	cout << "If you want to work with Integer enter 2" << endl;
+	cout << "If you want to work with Boolean, enter 3" << endl;
+	cout << "If you want to work with If enter 4" << endl;
+}
+
+void begincout()
+{
+	cout << "What task you want to solve?" << endl;
+	cout << "If you want solve task 'Begin 10', enter 10 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Begin 20', enter 20 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Begin 30', enter 30 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Begin 40', enter 40 and then 'Enter'." << endl;
+}
+
+void booleancout()
+{
+	cout << "What task you want to solve?" << endl;
+	cout << "If you want solve task 'Boolean 10', enter 10 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Boolean 20', enter 20 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Boolean 39', enter 39 and then 'Enter'." << endl;
+	cout << "If you want solve task 'Boolean 40', enter 40 and then 'Enter'." << endl;
+}
+
+void ifcout()
+{
+	cout << "What task you want to solve?" << endl;
+	cout << "If you want solve task 'If 10', enter 10 and then 'Enter'." << endl;
+	cout << "If you want solve task 'If 20', enter 20 and then 'Enter'." << endl;
+	cout << "If you want solve task 'If 30', enter 30 and then 'Enter'." << endl;
+}
+
+void knowcout()
+{
+	cout << "Do you want to know the condition of the task?" << endl;
+	cout << "If yes then enter 1, if no - 0." << endl;
+}
